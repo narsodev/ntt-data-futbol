a22098b7b59d83180475286c395cd9f1a64f6099
narsodev <narsodev@gmail.com>
2022-05-13 00:08:39 +0200

    Add files

---
+++ create_user.sql
@@ -0,0 +1,14 @@
+-- Eliminación de tablespace.
+alter session set "_ORACLE_SCRIPT"=true;
+DROP USER futbol_manager CASCADE;
+DROP TABLESPACE futbol_manager_tablespace INCLUDING CONTENTS AND DATAFILES CASCADE CONSTRAINTS;
+
+-- Creación de tablespace.
+CREATE TABLESPACE futbol_manager_tablespace DATAFILE 'C:\Users\Narciso\Desktop\Profesional\Oracle\oradata\XE\futbol_manager_tablespace.dbf' SIZE 10M AUTOEXTEND ON NEXT 10M MAXSIZE UNLIMITED;
+
+-- Generación de usuario, asignación de permisos sobre tablespace.
+alter session set "_ORACLE_SCRIPT"=true;
+CREATE USER futbol_manager IDENTIFIED BY rootroot DEFAULT TABLESPACE futbol_manager_tablespace;
+GRANT CREATE SESSION, ALTER SESSION, CREATE TABLE, CREATE SEQUENCE,
+CREATE TABLESPACE, UNLIMITED TABLESPACE TO futbol_manager;
+
---
+++ drop_tables.sql
@@ -0,0 +1,46 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+*                                                                     *
+*                           IES CAMPANILLAS                           *
+*                                                                     *
+*   - Alejandro Aguilera García                                       *
+*   - Narciso Gonzalez Calderón                                       *
+*   - Ernesto Hernandez Mangas                                        *
+*   - Alfonso Urbano Ruz                                              *
+*                                                                     *
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+-- Eliminar tabla gol
+DROP TABLE gol;
+
+-- Eliminar tabla jugador_partido
+DROP TABLE jugador_partido;
+
+-- Eliminar tabla ojeador_partido;
+DROP TABLE ojeador_partido;
+
+-- Eliminar tabla partido
+DROP TABLE partido;
+
+-- Eliminar tabla contrato_entrenador
+DROP TABLE contrato_entrenador;
+
+-- Eliminar tabla entrenador
+DROP TABLE entrenador;
+
+-- Eliminar tabla contrato_jugador
+DROP TABLE contrato_jugador;
+
+-- Eliminar tabla jugador
+DROP TABLE jugador;
+
+-- Eliminar tabla contrato_ojeador
+DROP TABLE contrato_ojeador;
+
+-- Eliminar tabla ojeador
+DROP TABLE ojeador;
+
+-- Eliminar tabla equipo
+DROP TABLE equipo;
+
+-- Eliminar tabla estadio
+DROP TABLE estadio;
