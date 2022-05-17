989b88be90b22e990851f582f799f31414d2399b
Alejandro <nailepsie@gmail.com>
2022-05-17 10:33:41 +0200

    Add comments alter-table

--- alter_table.sql
+++ alter_table.sql
@@ -9,11 +9,14 @@
 *                                                                     *
 \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+-- Hacer que el dni del jugador sea único
 ALTER TABLE jugador
 ADD CONSTRAINT dni_unico_jugador UNIQUE (DNIJug);
 
+-- Hacer que el dni del entrenador sea único
 ALTER TABLE entrenador
 ADD CONSTRAINT dni_unico_entrenador UNIQUE (DNIEnt);
 
+-- Hacer que el dni del ojeador sea único
 ALTER TABLE ojeador
 ADD CONSTRAINT dni_unico_ojeador UNIQUE (DNIOje);
